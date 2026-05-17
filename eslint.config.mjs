b4d4c85a9ebfa3b6788394b358808b4a9a07cf08
SemenKr <samkrekotyn@gmail.com>
2026-05-17 22:41:24 +0300

    SCRUM-17 chore: setup code quality checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,40 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
 
-const eslintConfig = defineConfig([
+export const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  {
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-nested-ternary': 'error',
 
-export default eslintConfig;
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+
+      'react/button-has-type': 'error',
+      'react/prop-types': 'off',
+    },
+  },
+  {
+    files: ['**/*.stories.tsx'],
+    rules: {
+      'no-console': 'off',
+    },
+  },
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  prettier,
+])
+
+export default eslintConfig
