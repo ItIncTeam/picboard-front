f8f51e3e69e7d2b957f0118411f234aba36ae7b4
zheton90 <jen.nikolenko@yandex.ru>
2026-05-28 22:25:51 +0300

    SCRUM-53 feat: fix input component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,17 @@
+import { Input } from '@/shared/ui/input/Input'
+import Logo from '@/shared/assets/icon/search.svg'
+
 export default function NotFound() {
-  return <h1>App Not Found</h1>
+  return (
+    <div style={{ backgroundColor: 'black' }}>
+      <Input
+        variant="defaultIcon"
+        placeholder="Not Found"
+        label="Not Found"
+        Icon={Logo}
+        disabled={true}
+      />
+      <h1>App Not Found</h1>
+    </div>
+  )
 }
