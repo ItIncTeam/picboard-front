ad69927552cbcca3d0718bdbd88435760bdfffdf
zheton90 <jen.nikolenko@yandex.ru>
2026-06-06 10:14:42 +0300

    FRONT-94 feat: modal

--- src/shared/assets/icon/index.ts
+++ src/shared/assets/icon/index.ts
@@ -6,4 +6,3 @@ export { default as SearchIcon } from './search.svg?react'
 export { default as GithubIcon } from '../image/social/github.svg?react'
 export { default as GoogleIcon } from '../image/social/google.svg?react'
 export { default as Close } from './close.svg?react'
-
