a14d93a3e9d1dcf095bb9e63451d4014b79585c1
zheton90 <jen.nikolenko@yandex.ru>
2026-06-09 23:04:27 +0300

    FRONT-94 feat: fix button

--- src/shared/ui/modal/Modal.stories.tsx
+++ src/shared/ui/modal/Modal.stories.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/shared/ui/button'
 import { Text } from '@/shared/ui/typography'
 
 import { Modal } from './Modal'
+import { EmailSentModal } from '@/features/auth/sign-up-form/ui'
 
 const meta = {
   title: 'Shared/Modal',
@@ -34,10 +35,11 @@ export const Basic: Story = {
       <>
         <Button onClick={openModalHandler}>Open modal</Button>
         <Modal {...args} open={showModal} onClose={closeModalHandler}>
-          <div style={{ width: 378, padding: '24px' }}>
-            <Text mb={24}>Modal content goes here.</Text>
-            <Button onClick={closeModalHandler}>OK</Button>
-          </div>
+          <EmailSentModal email="vsdb@dsfbdfb.by" open={showModal} onClose={closeModalHandler} />
+          {/*<div style={{ width: 378, padding: '24px' }}>*/}
+          {/*  <Text mb={24}>Modal content goes here.</Text>*/}
+          {/*  <Button onClick={closeModalHandler}>OK</Button>*/}
+          {/*</div>*/}
         </Modal>
       </>
     )
