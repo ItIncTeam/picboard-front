451127ae550b849715e03b3309595b365df8beb5
KseniyaPereguda <97020917+KseniyaPereguda@users.noreply.github.com>
2026-06-11 22:30:58 +0300

    FRONT-97 fix: update recovery expired screen assets and translations (#104)

--- src/shared/assets/image/sign-up/index.ts
+++ src/shared/assets/image/sign-up/index.ts
@@ -1,2 +1,3 @@
+export { default as ExpiredPasswordRecoveryImage } from '../expired-password-recovery.svg'
 export { default as ExpiredSignUpImage } from '../expired-sign-up.png'
 export { default as SuccesSignUpImage } from '../succes-sign-up.png'
