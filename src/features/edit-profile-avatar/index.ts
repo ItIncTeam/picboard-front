a120d1aa0891e07bc500a29ebd3a0abeeb57126f
KseniyaPereguda <97020917+KseniyaPereguda@users.noreply.github.com>
2026-09-26 15:10:11 +0300

    FRONT-171 add avatar draft preview (#185)
    * FRONT-171 add avatar draft preview
    
    * FRONT-171 fix avatar file input focus

---
+++ src/features/edit-profile-avatar/index.ts
@@ -0,0 +1,7 @@
+export { AvatarDraftPicker } from './ui/AvatarDraftPicker'
+export {
+  ACCEPTED_AVATAR_MIME_TYPES,
+  MAX_AVATAR_FILE_SIZE_BYTES,
+  type AvatarDraft,
+  type AvatarFileValidationError,
+} from './model/avatarDraft'
